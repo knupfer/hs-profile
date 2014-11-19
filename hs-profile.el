b7f8eaeed5e97b13bca8834ba2802f1d2cf23366
Florian Knupfer <fknupfer@gmail.com>
2014-11-19 17:11:33 +0100

    regexp to extract summary of funs

--- hs-profile.el
+++ hs-profile.el
@@ -1,14 +1,20 @@
 ;;; Code:
 
 (setq hs-profile-file "/home/quxbar/git/hs-profile/50.hs")
+(setq hs-profile-file-base "/home/quxbar/git/hs-profile/50")
 
 (defun hs-profile ()
   (interactive)
   (message buffer-file-name)
-  (call-process "ghc" nil nil nil "50" "-prof" "-fprof-auto" "-fprof-cafs")
-  (call-process (concat (file-name-directory hs-profile-file)
-			(file-name-base hs-profile-file))
-		nil nil nil "+RTS" "-p"))
+  (call-process "ghc" nil nil nil hs-profile-file "-prof" "-fprof-auto" "-fprof-cafs")
+  (call-process hs-profile-file-base
+		nil nil nil "+RTS" "-p")
+  (with-temp-buffer
+    (insert-file-contents (concat hs-profile-file-base ".prof"))
+    (let ((case-fold-search nil))
+      (re-search-forward "\\(\\(?:^[a-z].*\n\\)+\\)"))
+    (setq hs-profile-result (match-string 1))
+    ))
 
 
 (provide 'hs-profile)
