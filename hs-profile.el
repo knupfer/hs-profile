aaf7365d1f29034dd53b8d4974a1e1364d02eb87
Florian Knupfer <fknupfer@gmail.com>
2014-11-19 19:25:08 +0100

    work with current-buffer

--- hs-profile.el
+++ hs-profile.el
@@ -1,28 +1,54 @@
 ;;; Code:
 
-(setq hs-profile-file "/home/quxbar/git/hs-profile/50.hs")
-(setq hs-profile-file-base "/home/quxbar/git/hs-profile/50")
-
 (defun hs-profile ()
   (interactive)
-  (remove-overlays nil nil 'category 'hs-profile-overlay)
-  (call-process "ghc" nil nil nil hs-profile-file "-prof" "-fprof-auto" "-fprof-cafs")
-  (call-process hs-profile-file-base
-		nil nil nil "+RTS" "-p")
-  (with-temp-buffer
-    (insert-file-contents (concat hs-profile-file-base ".prof"))
-    (let ((case-fold-search nil))
-      (re-search-forward "\\(\\(?:^[a-z].*\n\\)+\\)"))
-    (setq hs-profile-result (split-string (match-string 1))))
-  (goto-char (point-min))
-  (while (and hs-profile-result
-	      (re-search-forward (concat "^" (pop hs-profile-result) " ") nil t))
-    (end-of-line)
-    (pop hs-profile-result)
-    (let ((ov (make-overlay (point) (point))))
-      (overlay-put ov 'category 'hs-profile-overlay)
-      (overlay-put ov 'before-string (concat " " (pop hs-profile-result)
-					     " " (pop hs-profile-result))))))
+  (let ((file (file-name-base))
+	(dir (file-name-directory (buffer-file-name)))
+	(result))
+    (call-process "ghc" nil nil nil (concat dir file)
+		  "-prof" "-fprof-auto" "-fprof-cafs")
+    (call-process (concat dir file)
+		  nil nil nil "+RTS" "-p")
+    (with-temp-buffer
+      (insert-file-contents (concat dir file ".prof"))
+      (let ((case-fold-search nil))
+	(re-search-forward "\\(\\(?:^[a-z].*\n\\)+\\)"))
+      (setq result (split-string (match-string 1))))
+    (remove-overlays nil nil 'category 'hs-profile-overlay)
+    (save-excursion
+      (while (< 3 (length result))
+	(goto-char (point-min))
+	(when
+	    (if (search "." (car result))
+		(let ((fun (split-string (pop result) "\\.")))
+		  (re-search-forward (concat "^" (car fun) " ") nil t)
+		  (re-search-forward (concat "^\\( +\\| +where +\\| +let +\\)"
+					     (cadr fun) " ") nil t))
+	      (re-search-forward (concat "^" (pop result) " ") nil t))
+	  (end-of-line)
+	  (pop result)
+	  (let ((ov (make-overlay (point) (point)))
+		(cpu (string-to-number (pop result)))
+		(ram (string-to-number (pop result))))
+	    (overlay-put ov 'category 'hs-profile-overlay)
+	    (overlay-put ov 'before-string
+			 (concat
+			  (make-string (max 0 (- 68 (current-column))) ?\s)
+			  (eval `(propertize
+				  (format "%6s" cpu)
+				  'face '(:weight bold :foreground
+						  ,(format "#%02X%02X%02X"
+							   (* 254 (sqrt (/ cpu 100)))
+							   (* 100 (sqrt (sqrt (/ cpu 100))))
+							   (* 50 (sqrt (/ cpu 50)))))))
+			  (eval `(propertize
+				  (format "%6s" ram)
+				  'face '(:weight bold :foreground
+						  ,(format "#%02X%02X%02X"
+							   (* 50 (sqrt (/ ram 50)))
+							   (* 100 (sqrt (sqrt (/ ram 100))))
+							   (* 254 (sqrt (/ ram 100)))))))))))))))
+
 
 (provide 'hs-profile)
 
